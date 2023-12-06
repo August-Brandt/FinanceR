f3f9654bdf9324c485786a0e8cb36620fab23af9
August Brandt <aubr@itu.dk>
2023-12-06 09:05:53 +0100

    Adds simple bar chart drawing in the console

---
+++ src/Graphmaker.py
@@ -0,0 +1,12 @@
+
+class GraphMaker:
+    def DrawBarChart(self, data: dict):
+        datasum = sum(data.values())
+        for dataname, datapoint in zip(data.keys(), data.values()):
+            print(dataname + "\t" + "#"*int(datapoint/datasum*50))
+
+
+if __name__ == "__main__":
+    graphMaker = GraphMaker()
+    data = {"1": 5, "2": 10, "3": 2, "4": 5, "5": 14, "6": 7}
+    graphMaker.DrawBarChart(data)
